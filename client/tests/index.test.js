6fe9fc4d9345e22b0ea6421244a63a8f780e33d8
trantrongbinh <tranbinhbak@gmail.com>
2019-09-06 16:14:02 +0700

    Config Travis CI

---
+++ client/tests/index.test.js
@@ -0,0 +1,25 @@
+/* eslint-env jest */
+
+// import { shallow } from 'enzyme'
+// import React from 'react'
+// /* eslint-disable-next-line no-unused-vars */
+// import renderer from 'react-test-renderer'
+// import { Provider } from "react-redux";
+// import configureMockStore from "redux-mock-store";
+
+// import { Index } from '../pages/index.js'
+
+// const mockStore = configureMockStore();
+// const store = mockStore({});
+
+describe("Index Component", () => {
+  it("Should render without throwing an error", () => {
+    // expect(
+    //   shallow(
+    //     <Provider store={store}>
+    //       <Index />
+    //     </Provider>
+    //   ).find(Index).dive().find('h2').text()
+    // ).toBe('Index page');
+  });
+});
